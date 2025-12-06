594b035dd0f64d14dc76c375e1b3804aaf2b912c
mikolaj-jalocha <mikics2k17@gmail.com>
2025-12-06 16:51:24 +0100

    init: setup project

---
+++ src/main/java/com/mj/springbootlicenseplates/SpringBootLicensePlatesApplication.java
@@ -0,0 +1,13 @@
+package com.mj.springbootlicenseplates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLicensePlatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLicensePlatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mj/springbootlicenseplates/SpringBootLicensePlatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj.springbootlicenseplates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLicensePlatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

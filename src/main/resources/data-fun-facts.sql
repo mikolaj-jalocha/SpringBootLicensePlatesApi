d1e547d4ee2946c28f14dc9df2b735e062be2b9f
mikolaj-jalocha <mikics2k17@gmail.com>
2025-12-08 20:49:25 +0100

    feat: add data for license plates and fun facts

---
+++ src/main/resources/data-fun-facts.sql
@@ -0,0 +1,4 @@
+INSERT INTO fun_facts (description,image_url,source) VALUES ('Ciekawostka 1 ',null,null);
+INSERT INTO fun_facts (description,image_url,source) VALUES ('Ciekawostka 2 ',null,null);
+INSERT INTO fun_facts (description,image_url,source) VALUES ('Ciekawostka 3',null,null);
+INSERT INTO fun_facts (description,image_url,source) VALUES ('Ciekawostka 4',null,null);
---
+++ src/main/resources/data-license-plates.sql
@@ -0,0 +1,4 @@
+INSERT INTO registration_plates (code,district_id,type_id,number_of_views) VALUES ('DX',1,1,0);
+INSERT INTO registration_plates (code,district_id,type_id,number_of_views) VALUES ('DKL',3,1,0);
+INSERT INTO registration_plates (code,district_id,type_id,number_of_views) VALUES ('RZ',22,1,0);
+INSERT INTO registration_plates (code,district_id,type_id,number_of_views) VALUES ('S1',29,2,0);

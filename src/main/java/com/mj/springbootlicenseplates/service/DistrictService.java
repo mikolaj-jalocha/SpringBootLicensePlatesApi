9191b9a2ccd3ad950ac6e269ad5c3150481671d7
majam <majamroczek.pl@gmail.com>
2025-12-17 10:46:33 +0100

    feat: create service for districts

---
+++ src/main/java/com/mj/springbootlicenseplates/service/DistrictService.java
@@ -0,0 +1,9 @@
+package com.mj.springbootlicenseplates.service;
+
+import com.mj.springbootlicenseplates.entity.District;
+
+import java.util.List;
+
+public interface DistrictService {
+    List<District> getAll();
+}
